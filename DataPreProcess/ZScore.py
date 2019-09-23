8dcd1075ad71f7b59ba01484f10a11d905e16b5d
sahanahmadjit <sahan.ahmad.jit@gmail.com>
2019-09-23 18:14:41 -0500

    Standard Deviation Calculation initial part Done

--- DataPreProcess/ZScore.py
+++ DataPreProcess/ZScore.py
@@ -1,10 +1,14 @@
 import csv
-import networkx as nx
+import math
 
 mac_data_directory= "/zWorkStation/JournalWork/Topic-Model/Data/"
 linux_data_directory="/home/C00408440/ZWorkStation/JournalVersion/Data/"
 
 zDictPreprocess = dict()
+frequencyMean= 0.0
+associationMean=0.0
+totalNumberOfTerm=0
+
 def createZScoreFormatData():
   for itaration in range(2):
     with open(linux_data_directory + 'GraphData/GraphInputData_Test.txt') as csv_file:
@@ -36,10 +40,52 @@ def writeFrequencyAssociationToFile():
     frequencyAssociationDatainputFile.write("\n")
 
 
+def meanCallculation(frequencyMean,associationMean,totalNumberOfTerm):
+      with open(linux_data_directory + 'GraphData/FrequencyAssociationInfo_test.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='|')
+
+        for line in csv_reader:
+          frequencyMean = frequencyMean + int(line[2])
+          associationMean = associationMean + int(line[4])
+          totalNumberOfTerm=totalNumberOfTerm+1
+
+        frequencyMean=frequencyMean/totalNumberOfTerm
+        associationMean=associationMean/totalNumberOfTerm
+        totalNumberOfTerm=totalNumberOfTerm
+      return frequencyMean,associationMean,totalNumberOfTerm
+
+
+def statisticalInfoWriteToFile(statiscalInfo):
+  statisticalWrite=open(linux_data_directory + "GraphData/StatisticalInfo_test.txt", "w")
+  statisticalWrite.write("MeanFrequency"+"|" + str(statiscalInfo[0])+ "|" + "MeanAssociation"+ "|" + str(statiscalInfo[1]) +"|"+"Total Number of Term"+"|"+str(statiscalInfo[2]))
+
+def calculateStandardDeviation(meanFrequency,meanAssociation,totalNumberOfTerm):
+  with open(linux_data_directory + 'GraphData/FrequencyAssociationInfo_test.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='|')
+    summationFrequencyDeviation=0.0
+    summationAssociaionDevaition=0.0
+    for line in csv_reader:
+      frequencyDeviation = float(line[2]) - meanFrequency
+      associaionDevaition =float(line[4]) - meanAssociation
+      frequencyDeviation = math.pow(frequencyDeviation,2)
+      associaionDevaition = math.pow(associaionDevaition,2)
+      summationFrequencyDeviation+= frequencyDeviation
+      summationAssociaionDevaition+=associaionDevaition
+
+    summationFrequencyDeviation /= totalNumberOfTerm
+    summationAssociaionDevaition/=totalNumberOfTerm
+    summationFrequencyDeviation=math.sqrt(summationFrequencyDeviation)
+    summationAssociaionDevaition=math.sqrt(summationAssociaionDevaition)
+  return summationFrequencyDeviation,summationAssociaionDevaition
+
+
+
 def main_ZScore():
   createZScoreFormatData()
   print_Dictonary()
-  writeFrequencyAssociationToFile()
-
-
+  #writeFrequencyAssociationToFile()
+  statiscalInfo=meanCallculation(frequencyMean,associationMean,totalNumberOfTerm)
+  print(statiscalInfo)
+  statisticalInfoWriteToFile(statiscalInfo)
+  print(calculateStandardDeviation(statiscalInfo[0],statiscalInfo[1],statiscalInfo[2]))
 main_ZScore()
