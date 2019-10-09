2a1932c96f6845016e5c4fba8c52e05367e10ff8
Sahan's Mac <tiger@Sahanss-iMac.local>
2019-10-09 02:28:34 -0500

    Lower Connectivity Checking Step Implemented

--- DataPreProcess/GraphBuilding.py
+++ DataPreProcess/GraphBuilding.py
@@ -1,5 +1,6 @@
 import csv
 import networkx as nx
+import math
 
 mac_data_directory= "/zWorkStation/JournalWork/Topic-Model/Data/"
 linux_data_directory="/home/C00408440/ZWorkStation/JournalVersion/Data/"
@@ -7,29 +8,105 @@ linux_data_directory="/home/C00408440/ZWorkStation/JournalVersion/Data/"
 G= nx.DiGraph()
 def building_graph_from_text_data():
   for itaration in range(2):
-    with open(linux_data_directory + 'GraphData/GraphInputData_Test.txt') as csv_file:
+    with open(mac_data_directory + 'GraphData/GraphInputData.txt') as csv_file:
       csv_reader = csv.reader(csv_file, delimiter='|')
+      limitLineForExperiment=0
       for line in csv_reader:
+        if limitLineForExperiment>10:
+          break
+        limitLineForExperiment+=1
         if itaration==0:
           G.add_node(line[0])
           continue
         else:
           for i in range(1,len(line),2): # 1:Starting from 2nd term {first term is source}, stop, increment by 2
             G.add_edge(line[0],line[i],weight=line[i+1])
+            #print("Processing Node for:" + line[0])
 
 def print_Graph():
-  print(G.edges(data='weight'))
+  print("Number of Nodes In Graph" + str(len(G)))
 
 def export_Graph_to_Gephi_format():
-  nx.write_gexf(G,linux_data_directory+"GraphData/gephi_format_graph_test.gexf")
+  nx.write_gexf(G,mac_data_directory+"GraphData/gephi_format_graph.gexf")
 
+
+def export_Lower_CommunityGraph_to_Gephi_format():
+  nx.write_gexf(G,mac_data_directory+"GraphData/gephi_format_LowerCommunityGraph.gexf")
+
+def disconnectTopTermInGraph():
+  with open(mac_data_directory + 'GraphData/zScoreSorted.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='|')
+    topTermNumber=0
+    for line in csv_reader:
+      G.remove_node(line[0])
+      if topTermNumber <=0:
+        break
+  print(G.edges(data='weight'))
+
+def lowerLevelConnectivityChecking():
+  logFile= open(mac_data_directory + 'GraphData/LowerConnectivityLogFile.txt',"w")
+  with open(mac_data_directory + 'GraphData/GraphInputData.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='|')
+    limitForExperiment=0
+    for line in csv_reader:
+      if limitForExperiment>10:
+        break
+      limitForExperiment+=1
+      neighborsNode= G[line[0]]
+      logFile.write("Soruce: "+ line[0] + "\n")
+      logFile.write("NeighborNode:" +"\n")
+      for nodes in neighborsNode:
+        logFile.write(nodes+" | ")
+      totalConnectedTerm=len(neighborsNode)
+      totalFrequency=0
+      for term in neighborsNode:
+        weightDict= neighborsNode[term]
+        for weightVal in weightDict:
+         totalFrequency+=int(weightDict[weightVal])
+      #Mean Devaition Calculation for the term
+      mean=float(totalFrequency/totalConnectedTerm)
+      logFile.write("\n" + "Mean: " + str(mean) +"\n")
+
+
+
+      SD_Value=deviationCalculation(mean,line[0],neighborsNode)
+      logFile.write("SD Value: "+str(SD_Value)+"\n")
+      removedEdgeList=[]
+      for term in neighborsNode:
+        weightDict= neighborsNode[term]
+        for weightVal in weightDict:
+         if int(weightDict[weightVal])<mean:
+           oneSDVal=mean-SD_Value
+           if int(weightDict[weightVal])<oneSDVal:
+             removedEdgeList.append(term)
+      for removeTerm in removedEdgeList:
+        G.remove_edge(line[0],removeTerm)
+        logFile.write("Edges Remove From "+ line[0]+ " to "+ removeTerm+"\n")
+
+
+def deviationCalculation(mean,sourceTerm,neighborsNode):
+  meanDeviationSummation=0.0
+  standardDeviationSummation=0.0
+  for term in neighborsNode:
+    weightDict=neighborsNode[term]
+    for weightVal in weightDict:
+      deviation=abs(int(weightDict[weightVal])-mean)
+      meanDeviationSummation+=deviation
+      standardDeviationSummation+= math.pow(deviation,2)
+  meanDeviation=meanDeviationSummation/len(neighborsNode)
+  standardDeviation=math.sqrt(standardDeviationSummation/len(neighborsNode))
+  return standardDeviation
 
 building_graph_from_text_data()
 
 def main_Graph_Building_function():
   building_graph_from_text_data()
-  print_Graph()
   export_Graph_to_Gephi_format()
+  print_Graph()
+#  disconnectTopTermInGraph()
+  lowerLevelConnectivityChecking()
+  export_Lower_CommunityGraph_to_Gephi_format()
+  #print_Graph()
 
 
 main_Graph_Building_function()
---
+++ DataPreProcess/StrongConnectivity.py
@@ -0,0 +1,10 @@
+import csv
+import math
+import operator
+import matplotlib.pyplot as plt
+
+mac_data_directory= "/zWorkStation/JournalWork/Topic-Model/Data/"
+linux_data_directory="/home/C00408440/ZWorkStation/JournalVersion/Data/"
+
+
+
